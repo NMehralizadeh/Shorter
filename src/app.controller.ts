7a21071b2267a7f5550d24b471bca5d38b849a09
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-05-12 15:21:37 +0430

    Add redirector action

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,39 @@
-import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Req, Get, Post, UseGuards, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { UrlService } from './url/url.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private authService: AuthService,
+    private urlService: UrlService,
   ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('r/*')
+  getHello(@Req() request: Request, @Res() response: Response) {
+    //Log user request and information like IP, Browser, OS,
+    const redirectedUrl = this.urlService.getMainUrlByUniqueUrl(
+      request.originalUrl.split('/')[2],
+    );
+    if (redirectedUrl) {
+      response.redirect(redirectedUrl);
+    } else {
+      response.statusCode = 404;
+    }
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Req() request: Request) {
+    return this.authService.login(request.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
+  getProfile(@Req() req: Request) {
     return req.user;
   }
 }
