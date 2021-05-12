c4e6d0e47a82c80f6a60590c5895ceffa606a136
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-05-12 23:56:49 +0430

    Add Config System

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UrlController } from './url/url.controller';
 import { UrlService } from './url/url.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'config/configuration';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController, UrlController],
   providers: [AppService, UrlService],
 })
