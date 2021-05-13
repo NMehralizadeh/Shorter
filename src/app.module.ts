d6081bd44ad311bad212b63c9b01d1372cbe5474
Navid Mehralizadeh <navid.mehralizadeh@outlook.com>
2021-05-14 00:34:09 +0430

    Create Admin user in first time

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { UrlController } from './url/url.controller';
 import { UrlService } from './url/url.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -15,8 +17,21 @@ import configuration from 'config/configuration';
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
   ],
   controllers: [AppController, UrlController],
   providers: [AppService, UrlService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private usersSerive: UsersService) {
+    this.createAdminUser();
+  }
+
+  createAdminUser() {
+    this.usersSerive.getByUsername('admin').then((user) => {
+      if (!user) {
+        this.usersSerive.createAdminUser();
+      }
+    });
+  }
+}
